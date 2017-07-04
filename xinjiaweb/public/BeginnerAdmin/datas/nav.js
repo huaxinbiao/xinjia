49606fe462fccf9214d0f818ddf89f7dbdfa843b
huaxinbiao <1960067872@qq.com>
2017-07-04 22:43:39 +0800

    逻辑整理

--- xinjiaweb/public/BeginnerAdmin/datas/nav.js
+++ xinjiaweb/public/BeginnerAdmin/datas/nav.js
@@ -1,9 +1,21 @@
 var navs = [{
-	"title": "文章管理",
+	"title": "分类管理",
 	"icon": "fa-cubes",
 	"spread": true,
 	"children": [{
-		"title": "文章分类",
+		"title": "添加分类",
+		"icon": "&#xe609;",
+		"href": "nav.html"
+	}, {
+		"title": "分类列表",
+		"icon": "&#xe63c;",
+		"href": "form.html"
+	}]
+},{
+	"title": "文章管理",
+	"icon": "fa-cubes",
+	"children": [{
+		"title": "发布文章",
 		"icon": "&#xe609;",
 		"href": "nav.html"
 	}, {
@@ -11,4 +23,19 @@ var navs = [{
 		"icon": "&#xe63c;",
 		"href": "form.html"
 	}]
+},{
+	"title": "网站模块设置",
+	"icon": "fa-cubes",
+	"children": [{
+		"title": "成功案例设置",
+		"icon": "&#xe609;",
+		"href": "nav.html"
+	}, {
+		"title": "实用信息设置",
+		"icon": "&#xe63c;",
+		"href": "form.html"
+	}]
+},{
+	"title": "关于我们",
+	"icon": "fa-cubes"
 }];
